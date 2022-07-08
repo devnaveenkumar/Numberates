a37dfd7e2539bf9bfba80e796d2d8c2161adba95
devnaveenkumar <98394930+devnaveenkumar@users.noreply.github.com>
2022-07-08 11:09:10 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+var guess = Math.floor(Math.random()*100+1);
+var output = document.getElementById('textLabel');
+
+var random = document.getElementById('attemptCount')
+var guessCount = 10;
+var i;
+function check() {
+
+    if (guessCount != 0) {
+        var num = document.getElementById('textBox').value;
+    
+    if(num==guess) {
+        location.href = "win.html";
+    }
+    else if(num>guess) {
+        output.innerHTML="Your guess is too high!";
+    }
+    else if(num<guess) {
+        output.innerHTML="Your guess is too low!";
+    }
+    else if(isNaN(num)) {
+        output.innerHTML="That is not a number";
+    }
+    guessCount--;
+    random.innerHTML= guessCount;
+    }
+    else if (guessCount == 0) {
+        random.innerHTML="Oops you are out of guesses!";
+        return;
+    }
+}
+    
